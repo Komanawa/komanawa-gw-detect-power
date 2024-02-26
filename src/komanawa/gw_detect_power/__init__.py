25184881e90a5edcb5188e1165858653ee1e23e5
matt_dumont <hansonmcoombs@gmail.com>
2024-02-27 09:06:22 +1300

    refactor for organsiation namespace

--- src/komanawa/gw_detect_power/__init__.py
+++ src/komanawa/gw_detect_power/__init__.py
@@ -3,7 +3,6 @@
 created matt_dumont
 on: 6/07/23
 """
-# todo relative imports are not working...
-from gw_detect_power.change_detection_counterfactual import DetectionPowerCounterFactual, \
+from komanawa.gw_detect_power.change_detection_counterfactual import DetectionPowerCounterFactual, \
     AutoDetectionPowerCounterFactual
-from gw_detect_power.change_detection_slope import DetectionPowerSlope, AutoDetectionPowerSlope, DetectionPowerCalculator
+from komanawa.gw_detect_power.change_detection_slope import DetectionPowerSlope, AutoDetectionPowerSlope, DetectionPowerCalculator
